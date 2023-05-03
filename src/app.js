8083f51018884f8d6efaa727ef9c4dfb07e2294d
Craig <c.chick@tx-consulting.com>
2023-05-03 03:12:12 +0200

    Added fonts and colors scheme

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='bg-third'>
+      <Text className='text-first text-5xl'>Hello</Text>
+      <Text className='text-first text-8xl'>Hello</Text>
+      <Text className='text-dark text-7xl'>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+    </View>
+  );
+}
